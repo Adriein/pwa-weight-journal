eb5fc7906ef8678044fb899759ab4b0482d96398
Adriein <adria.claret@gmail.com>
2020-08-18 17:18:16 +0200

    added transitions

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -82,15 +82,15 @@ export default function Login() {
   return (
     <div className="h-screen flex flex-col justify-start items-center p-4">
       <div
-        className={`fixed z-20 inset-0 bg-black opacity-50 ${
-          open ? 'transition duration-1000 ease-in' : 'hidden'
+        className={`fixed z-20 inset-0 bg-black ${
+          open ? 'transition duration-500 ease-in opacity-50 ' : 'opacity-0'
         }`}
         onClick={() => setOpen(!open)}
       ></div>
       <div className="absolute inset-x-0 top-0 h-64 bg-blue-500 shadow-md">
         <div
           className={`absolute top-0 right-0 w-8 h-8 mt-3 mr-2 rounded-full z-20 ${
-            open ? 'transition duration-1000 ease-in bg-blue-200' : ''
+            open ? 'transition duration-500 ease-in bg-blue-200' : ''
           }`}
           onClick={() => setOpen(!open)}
         >
@@ -100,7 +100,7 @@ export default function Login() {
         </div>
         <div
           className={`absolute rounded w-32 h-8 bg-blue-100 top-0 right-0 p-2 mt-12 mr-3 flex-auto justify-center shadow-md ${
-            open ? 'z-20' : 'hidden'
+            open ? 'transition duration-500 ease-in z-20 opacity-100' : 'opacity-0'
           }`}
         >
           <p
