0aefe83c4549b50667a3a31791889718eacee609
Adriein <adria.claret@gmail.com>
2020-08-19 19:05:15 +0200

    no message

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -28,7 +28,7 @@ export default function Header({ currentPage, navigation, goBack }) {
     }
   };
   return (
-    <header className="flex items-center p-2">
+    <header className="flex items-center p-4 bg-white">
       <div
         className={`fixed z-10 inset-0 bg-black ${
           open
@@ -37,42 +37,44 @@ export default function Header({ currentPage, navigation, goBack }) {
         }`}
         onClick={() => setOpen(!open)}
       ></div>
-      {goBack && (
-        <button className="focus:outline-none focus:appearance-none focus:border-none active:outline-none active:appearance-none outline-none appearance-none w-10 h-10 rounded-full bg-gray-200 mr-10">
+      {navigation && (
+        <button className="focus:outline-none focus:appearance-none focus:border-none active:outline-none active:appearance-none outline-none appearance-none w-10 h-10 rounded-full bg-gray-200 mr-5">
           <MdKeyboardArrowLeft className="w-full h-full text-gray-500" />
         </button>
       )}
-      <h2 className="flex-grow font-bold text-3xl text-blue-900 ml-2">
+      <h2 className="flex-grow font-bold text-3xl text-blue-700">
         {currentPage}
       </h2>
-      <button
-        className={`relative z-20 focus:outline-none focus:appearance-none w-12 h-12 rounded-full overflow-hidden border-2 border-gray-400 ${
-          open &&
-          'outline-none border-3 border-blue-100 focus:outline-none focus:appearance-none'
-        }`}
-        onClick={() => setOpen(!open)}
-      >
-        <img
-          alt="Logo"
-          className="h-full w-full object-cover"
-          src="/api/static/2"
-        />
-      </button>
-      {open && (
-        <div className="absolute z-20 rounded-lg top-0 right-0 p-1 bg-gray-200 mt-16 mr-5 p-1">
-          <button className="w-full p-2 flex items-center focus:outline-none focus:appearance-none active:bg-white">
-            <p className="text-base">Perfil</p>
-            <MdSettings className="ml-2" />
-          </button>
-          <button
-            className="w-full p-2 flex items-center focus:outline-none focus:appearance-none"
-            onClick={handleLogout}
-          >
-            <p className="text-base">Salir</p>
-            <MdExitToApp className="ml-2" />
-          </button>
-        </div>
-      )}
+      <div className="relative z-20 ">
+        <button
+          className={`focus:outline-none focus:appearance-none w-12 h-12 rounded-full overflow-hidden border-2 border-gray-400 ${
+            open &&
+            'outline-none border-3 border-blue-100 focus:outline-none focus:appearance-none'
+          }`}
+          onClick={() => setOpen(!open)}
+        >
+          <img
+            alt="Logo"
+            className="h-full w-full object-cover"
+            src="/api/static/2"
+          />
+        </button>
+        {open && (
+          <div className="absolute z-20 rounded-lg right-0 p-1 bg-gray-200 p-1 mr-4">
+            <button className="w-full p-2 flex items-center focus:outline-none focus:appearance-none active:bg-white">
+              <p className="text-base">Perfil</p>
+              <MdSettings className="ml-2" />
+            </button>
+            <button
+              className="w-full p-2 flex items-center focus:outline-none focus:appearance-none"
+              onClick={handleLogout}
+            >
+              <p className="text-base">Salir</p>
+              <MdExitToApp className="ml-2" />
+            </button>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
