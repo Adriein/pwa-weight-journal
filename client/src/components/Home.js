0dac21b2d2b673e2e4033996e3bd1aa233b4fda9
Adriein <adria.claret@gmail.com>
2020-08-22 18:55:32 +0200

    made carousel reusable component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import { ENV } from '../helpers';
+import React, { useRef } from 'react';
+import { motion } from 'framer-motion';
+import { useHistory } from 'react-router-dom';
+
+import { MdSettings } from 'react-icons/md';
 
 import Header from './Header';
 import Navigation from './Navigation';
 import Carousel from './Carousel';
 
 export default function Home() {
+  const history = useHistory();
+  const targetRef = useRef();
+  const navigate = (path) => () => {
+    history.push(`/${path}`);
+  };
+  const CONTENT = {
+    logs: {
+      title: 'Inicia tu entrenamiento',
+      legend: 'Empieza a medir la calidad de tus entrenos',
+    },
+    graphs: {
+      title: 'Mide tu progreso',
+      legend: 'Graficos actualizados de tus registros de entrenamiento',
+    },
+    settings: {
+      title: 'Configura tu cuenta',
+      legend: 'Añade una foto de perfil y configura tu cuenta',
+    },
+  };
   return (
     <div>
       <Header currentPage={'Inicio'} />
       <div>
-        {/* <div className="absolute bg-white opacity-75 flex">
-          <p className="font-medium text-lg px-4">
-            Bienvenido {localStorage.getItem(ENV.username)}, inicia tu entreno o
-            consulta tus estadisticas
-          </p>
-        </div> */}
-
-        {/* <img src="/api/static/3" alt="home gym" /> */}
-        <Carousel />
+        <Carousel>
+          {[
+            ['logs', 'blue'],
+            ['graphs', 'green'],
+            ['settings', 'yellow'],
+          ].map(([link, color], index) => {
+            return (
+              <motion.div
+                className={`bg-${color}-400 w-48 h-40 mr-5 rounded-md p-2 flex-col`}
+                onClick={navigate(link)}
+                whileTap={{ scale: 1.1 }}
+                ref={targetRef}
+              >
+                {index === 2 ? (
+                  <div className="w-12 h-12 rounded-full overflow-hidden bg-gray-200">
+                    <MdSettings className="h-full w-full object-cover text-yellow-600" />
+                  </div>
+                ) : (
+                  <div className="w-12 h-12 rounded-full overflow-hidden">
+                    <img
+                      className="h-full w-full object-cover"
+                      src={`/api/static/${index + 3}`}
+                      alt="training"
+                    />
+                  </div>
+                )}
+                <div className="p-1 mt-5">
+                  <h4 className="text-sm font-semibold">
+                    {CONTENT[link].title}
+                  </h4>
+                  <p className="text-xs">{CONTENT[link].legend}</p>
+                </div>
+              </motion.div>
+            );
+          })}
+        </Carousel>
       </div>
       <Navigation active={'home'} />
     </div>
--- client/src/components/RegisterBook.js
+++ client/src/components/RegisterBook.js
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import { motion } from 'framer-motion';
 import { traduceCategories } from '../helpers';
 import { useHistory } from 'react-router-dom';
 
@@ -8,112 +9,51 @@ import useCounter from '../hooks/useCounter';
 import { ExerciceContext } from '../context/ExerciceContext';
 import { DispatchContext } from '../context/ExerciceContext';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Slide from '@material-ui/core/Slide';
-import Grow from '@material-ui/core/Grow';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-
 import 'date-fns';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
+import { MdSettings } from 'react-icons/md';
 
 import Navigation from './Navigation';
 import SearchBar from './SearchBar';
-import LogCard from './LogCard';
-// import InfoCard from './InfoCard';
+import Carousel from './Carousel';
 import Header from './Header';
 
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: 'none',
-    },
-  },
-  container: {
-    padding: theme.spacing(2, 2, 2, 2),
-    backgroundColor: '#F0F4F8',
-  },
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  footer: {
-    marginTop: 'auto',
-  },
-  error: {
-    marginTop: theme.spacing(5),
-  },
-  item: {
-    marginBottom: theme.spacing(4),
-  },
-  info: {
-    marginTop: theme.spacing(6),
-  },
-  categories: {
-    marginTop: theme.spacing(1),
-  },
-  categoryContainer: {
-    padding: theme.spacing(3),
-    minWidth: '100%',
-    backgroundColor: '#ffffff',
-  },
-  title: {
-    color: '#212429',
-  },
-  categoryList: {
-    marginBottom: theme.spacing(2),
-  }
-}));
-
 export default function RegisterBook() {
-  const classes = useStyles();
-  const history = useHistory();
+  // const history = useHistory();
   const exercices = useContext(ExerciceContext);
   const exerciceDispatcher = useContext(DispatchContext);
-  const [value, , , setValue] = useInputState({
-    date: new Date(),
-  });
+  // const [value, , , setValue] = useInputState({
+  //   date: new Date(),
+  // });
 
-  const handleDateChange = (date) => {
-    setValue(Object.assign({}, value, { date }));
-  };
+  // const handleDateChange = (date) => {
+  //   setValue(Object.assign({}, value, { date }));
+  // };
 
-  const [counter, modifyCounter, reset] = useCounter({
-    series: 0,
-    reps: 0,
-    kg: 0,
-  });
+  // const [counter, modifyCounter, reset] = useCounter({
+  //   series: 0,
+  //   reps: 0,
+  //   kg: 0,
+  // });
 
-  const discard = () => {
-    setValue(Object.assign({}, value, { date: new Date() }));
-    reset();
-    exerciceDispatcher({ type: 'DISCARD_SELECTION' });
-  };
+  // const discard = () => {
+  //   setValue(Object.assign({}, value, { date: new Date() }));
+  //   reset();
+  //   exerciceDispatcher({ type: 'DISCARD_SELECTION' });
+  // };
 
-  const saveLog = async () => {
-    const response = await axios.post(
-      '/api/log',
-      Object.assign(
-        {},
-        value,
-        { stats: counter },
-        { exerciceId: exercices.selected.id }
-      )
-    );
-    console.log(response);
-    exerciceDispatcher({ type: 'DISCARD_SELECTION' });
-  };
+  // const saveLog = async () => {
+  //   const response = await axios.post(
+  //     '/api/log',
+  //     Object.assign(
+  //       {},
+  //       value,
+  //       { stats: counter },
+  //       { exerciceId: exercices.selected.id }
+  //     )
+  //   );
+  //   console.log(response);
+  //   exerciceDispatcher({ type: 'DISCARD_SELECTION' });
+  // };
 
   // const handleRedirect = () => {
   //   history.push('/create-exercice');
@@ -128,114 +68,68 @@ export default function RegisterBook() {
     })();
   }, []);
 
-  const clickCategory = (event) => {
-    history.push('/category');
-    exerciceDispatcher({
-      type: 'LOADING',
-    });
-    (async () => {
-      exerciceDispatcher({
-        type: 'FETCH_CATEGORY',
-        payload: {
-          category: event.currentTarget.id,
-          exercices: (
-            await axios.get(`/api/category/${event.currentTarget.id}`)
-          ).data,
-        },
-      });
-    })();
-  };
+  // const clickCategory = (event) => {
+  //   history.push('/category');
+  //   exerciceDispatcher({
+  //     type: 'LOADING',
+  //   });
+  //   (async () => {
+  //     exerciceDispatcher({
+  //       type: 'FETCH_CATEGORY',
+  //       payload: {
+  //         category: event.currentTarget.id,
+  //         exercices: (
+  //           await axios.get(`/api/category/${event.currentTarget.id}`)
+  //         ).data,
+  //       },
+  //     });
+  //   })();
+  // };
+  const [isStarted, setStarted] = useState(false);
+  const targetRef = useRef();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Header currentPage={'Registros'} />
-      <Container maxWidth="md" component="main" className={classes.container}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <Grow in={!exercices.selected ? true : false} timeout={1000}>
-            <Grid container justify="center" spacing={2} className={classes.categoryList}>
-              {!exercices.selected && (
-                <>
-                  <Grid item xs={12}>
-                    <Typography
-                      variant="h6"
-                      gutterBottom
-                      className={classes.title}
-                    >
-                      Busca por nombre
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <SearchBar className={classes.item} />
-                  </Grid>
+    <div>
+      <Header currentPage={'Entreno'} />
 
-                  <Grid item xs={12}>
-                    <Typography
-                      variant="h6"
-                      gutterBottom
-                      className={classes.title}
-                    >
-                      Categoria
-                    </Typography>
-                  </Grid>
-                  {exercices.categories.map((category) => {
-                    return (
-                      <Grid item xs={12} key={category}>
-                        <Paper
-                          elevation={1}
-                          className={classes.categoryContainer}
-                          id={category}
-                          onClick={clickCategory}
-                        >
-                          {traduceCategories(category)}
-                        </Paper>
-                      </Grid>
-                    );
-                  })}
-                </>
-              )}
-            </Grid>
-          </Grow>
-          <Grid container justify="center">
-            {exercices.selected && (
-              <>
-                <Slide direction="down" in={exercices.selected ? true : false}>
-                  <Grid item>
-                    <KeyboardDatePicker
-                      margin="normal"
-                      name="date"
-                      label="Selecciona una fecha"
-                      format="dd/MM/yyyy"
-                      value={value.date}
-                      onChange={handleDateChange}
-                      KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                      }}
-                      className={classes.item}
-                    />
-                  </Grid>
-                </Slide>
+      <div className="px-4 flex justify-center">
+        {!isStarted && (
+          <button
+            className="focus:outline-none focus:appearance-none focus:border-none active:outline-none active:appearance-none active:border-none rounded-full active:rounded-full focus:rounded-full bg-blue-800 w-40 p-2 text-white text-xl font-semibold mt-4 mb-1"
+            onClick={() => setStarted(!isStarted)}
+          >
+            Iniciar Entreno
+          </button>
+        )}
+        {isStarted && (
+          <div className="w-full">
+            <p>Selecciona el ejercicio</p>
+            <SearchBar />
+            <Carousel>
+              {exercices.categories.map((category) => {
+                return (
+                  <motion.div
+                    className={`bg-gray-400 w-48 h-40 mr-5 rounded-md p-2 flex-col`}
+                    whileTap={{ scale: 1.1 }}
+                    ref={targetRef}
+                    key={category}
+                  >
+                    <div className="w-12 h-12 rounded-full overflow-hidden bg-gray-200">
+                      <MdSettings className="h-full w-full object-cover text-gray-600" />
+                    </div>
 
-                <Grow in={exercices.selected ? true : false} timeout={1000}>
-                  <Grid item>
-                    <LogCard
-                      counter={counter}
-                      modifyCounter={modifyCounter}
-                      discard={discard}
-                      saveLog={saveLog}
-                    />
-                  </Grid>
-                </Grow>
-              </>
-            )}
-          </Grid>
-        </MuiPickersUtilsProvider>
-      </Container>
-      <footer className={classes.footer}>
-        <Box mt={4}>
-          <Navigation active={'logs'} />
-        </Box>
-      </footer>
+                    <div className="p-1 mt-5">
+                      <h4 className="text-sm font-semibold">{category}</h4>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </Carousel>
+          </div>
+        )}
+      </div>
+
+      <Navigation active={'logs'} />
     </div>
   );
 }
